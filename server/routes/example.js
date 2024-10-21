5d13e0c37848443f88c30adbbda3feb06045e6d2
rachit-walia <rachitwalia3@gmail.com>
2024-10-22 00:35:47 +0530

    installed nodemon

--- server/routes/example.js
+++ server/routes/example.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 // Define your routes here
 router.get('/', (req, res) => {
-  res.send('Example route is working');
+  res.send('Example route is working ');
 });
 
 module.exports = router;
