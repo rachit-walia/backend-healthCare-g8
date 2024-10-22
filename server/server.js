e4a7580d6e0d12976a4adddc0cc7bd724560d657
rachit-walia <rachitwalia3@gmail.com>
2024-10-22 14:14:03 +0530

    .

--- server/server.js
+++ server/server.js
@@ -8,7 +8,8 @@ const exampleRoute = require('./routes/example');
 // Middleware to use the example route
 app.use('/example', exampleRoute);
 app.use(express.json());
-app.use(cors());
+app.use(cors()); // cors is used as a security feature in javascript that allows web application running in one domain to make request to resource in another domain
+
 
 // Start the server on a different port
 const port = process.env.PORT || 8080;  // Changed to port 8080
