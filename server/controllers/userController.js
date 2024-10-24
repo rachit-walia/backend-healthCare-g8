54d3a778369a560bd24dcced21c05c84d22088af
rachit-walia <rachitwalia3@gmail.com>
2024-10-24 14:28:13 +0530

    added npm async handler and bcrypt

---
+++ server/controllers/userController.js
@@ -0,0 +1,8 @@
+const asyncHandler=require("express-async-handler");
+const bcrypt=require("bcrypt");
+const user=require("../models/userModel");
+require("dotenv").config();
+
+const registerUser=asyncHandler(async(res,req)=>{
+
+});
