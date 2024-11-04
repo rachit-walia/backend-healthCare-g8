1b4094b3599f75bef18a5ec72e5bc6d9586f626d
rachit-walia <rachitwalia3@gmail.com>
2024-11-04 15:12:19 +0530

    modified DoctorRoutes

--- server/routes/doctorRoutes.js
+++ server/routes/doctorRoutes.js
@@ -2,13 +2,13 @@ const express = require("express");
 const router = express.Router();
 const { registerDoctor, getAllDoctors, getDoctorById } = require("../controllers/doctorController");
 
-// Register a new doctor
+// Define `/register` route before `/:id` to avoid route conflicts
 router.post("/register", registerDoctor);
 
 // Get all doctors
 router.get("/", getAllDoctors);
 
-// Get a doctor by ID
+// Get a doctor by ID (define after `/register` to avoid conflicts)
 router.get("/:id", getDoctorById);
 
 module.exports = router;
