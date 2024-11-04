b096d2bd0c96d12dd3212b2c62a9b204728d3ad4
rachit-walia <rachitwalia3@gmail.com>
2024-11-04 15:09:52 +0530

    created getAll and get by id in doctor

--- server/controllers/doctorController.js
+++ server/controllers/doctorController.js
@@ -32,4 +32,22 @@ const registerDoctor = asyncHandler(async (req, res) => {
   });
 });
 
-module.exports = { registerDoctor };
+// Get all doctors
+const getAllDoctors = asyncHandler(async (req, res) => {
+  const doctors = await Doctor.find();
+  res.status(200).json(doctors);
+});
+
+// Get doctor by ID
+const getDoctorById = asyncHandler(async (req, res) => {
+  const doctor = await Doctor.findById(req.params.id);
+
+  if (!doctor) {
+    res.status(404);
+    throw new Error("Doctor not found");
+  }
+
+  res.status(200).json(doctor);
+});
+
+module.exports = { registerDoctor, getAllDoctors, getDoctorById };
--- server/routes/doctorRoutes.js
+++ server/routes/doctorRoutes.js
@@ -1,7 +1,14 @@
 const express = require("express");
 const router = express.Router();
-const { registerDoctor } = require("../controllers/doctorController");
+const { registerDoctor, getAllDoctors, getDoctorById } = require("../controllers/doctorController");
 
+// Register a new doctor
 router.post("/register", registerDoctor);
+
+// Get all doctors
+router.get("/", getAllDoctors);
+
+// Get a doctor by ID
+router.get("/:id", getDoctorById);
 
 module.exports = router;
